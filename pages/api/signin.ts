ed7d9223f3a6f41ecb1bc66befc1b62cd880117a
yassinechayrrou <yassine.chayrrou@gmail.com>
2022-10-26 09:43:59 +0100

    signin api

---
+++ pages/api/signin.ts
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../../db/prisma";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUniqueOrThrow({ where: { email } });
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new Error("Invalid password");
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        timestamp: Date.now(),
+      },
+      process.env.SECRET,
+      { expiresIn: "8h" }
+    );
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+
+    res.status(200).json({ id: user.id, name: user.name, email: user.email });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: "Incorrect email address or password" });
+  }
+}
